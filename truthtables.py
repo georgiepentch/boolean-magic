4df5ae70ce1c436ca903bbdf2627413d50c4ce0e
George Pentchev <georgiepentch@gmail.com>
2024-03-27 03:16:56 -0400

    implemented TruthTables

---
+++ truthtables.py
@@ -0,0 +1,136 @@
+# libraries
+from string import ascii_uppercase
+import numpy as np
+from tabulate import tabulate as tb
+from inspect import signature
+from itertools import chain, combinations
+
+
+def powerset(iterable):
+    s = list(iterable)
+    return chain.from_iterable(combinations(s, r) for r in range(1, len(s)+1))
+
+
+# DECIMAL DIGIT —> 7-SEGMENT DISPLAY
+dig_to_segs = {
+    0: ['a', 'b', 'c', 'd', 'e', 'f'],
+    1: ['b', 'c'],
+    2: ['a', 'b', 'g', 'e', 'd'],
+    3: ['a', 'b', 'g', 'c', 'd'],
+    4: ['f', 'g', 'b', 'c'],
+    5: ['a', 'f', 'g', 'c', 'd'],
+    6: ['a', 'f', 'g', 'c', 'd', 'e'],
+    7: ['a', 'b', 'c'],
+    8: ['a', 'b', 'c', 'd', 'e', 'f', 'g'],
+    9: ['a', 'f', 'g', 'c', 'b']
+}
+
+# 7-SEGMENT DISPLAY —> DECIMAL DIGIT
+segs_to_dig = {k: [] for k in dig_to_segs[8]}
+for digs, segs in dig_to_segs.items():
+    for seg in segs:
+        segs_to_dig[seg] += [digs]
+
+
+class TruthTable:
+    def __init__(self, out, padding='x', var_names=ascii_uppercase):
+        """
+        :param out: A string of '1' (True), '0' (False), and 'x' (Either) characters.
+        :param padding: If len(out) != 2^n, which of '1', '0', or 'x' is used to pad the remaining output space.
+        :param var_names: String/list of characters/names to be used as variable names in tables and formulas.
+        """
+        self.p = int(np.ceil(np.log2(len(out))))  # num of input cols
+        self.entries = 1 << self.p  # num of rows
+        self.out = out + (self.entries - len(out)) * padding
+        self.var_names = var_names
+        self.expression = None
+
+    def table(self, tablefmt="fancy_outline"):
+        """:param tablefmt: The format of the table, as given by the 'tabulate' package."""
+        varr = []
+        for i in range(self.entries):
+            varr.append(list(format(i, "0" + str(self.p) + "b")))
+        varr = np.append(varr, [[k] for k in self.out], axis=1)
+        print(tb(varr, list(self.var_names[:self.p]) + [self.expression], tablefmt=tablefmt))
+
+    @classmethod
+    def from_expression(cls, f):
+        """:param f: A boolean-valued expression."""
+        p = f.__code__.co_argcount
+        out = ''
+        for i in range(1 << p):
+            out += str(int(f(*[int(q) for q in (format(i, "0" + str(p) + "b"))])))
+        var_names = list(signature(f).parameters)
+        return cls(out, var_names=var_names)
+
+    def qmc(self):
+        out1 = self.out.replace('x', '1')
+        minterms = [([], i, [i]) for i, b in enumerate(out1) if b == '1']
+        # minterms = [(list of bits replaced by '-', minterm value when '-' = '0', list of minterms grouped together)]
+        iteration = -1
+        x = 0
+        while len(minterms) > x:
+            iteration += 1
+            x = len(minterms)
+            for i in range(x):
+                if len(minterms[i][0]) < iteration:
+                    continue
+                for j in range(i + 1, x):
+                    t = minterms[i][1] ^ minterms[j][1]
+                    if set(minterms[i][0]) == set(minterms[j][0]) and t.bit_count() == 1:
+                        minterms.append((minterms[i][0] + [t], ~t & minterms[i][1], minterms[i][2] + minterms[j][2]))
+
+        implicants = [s[2] for s in reversed(minterms)]
+        numbers = set([s for term in minterms for s in term[2]])
+        prime_implicants = []
+        while numbers:
+            prime_implicants.append(implicants[0])
+            numbers -= set(implicants[0])
+            implicants = [s for s in implicants if numbers.intersection(set(s))]
+
+        out0 = self.out.replace('x', '0')
+        numbers = [i for i, b in enumerate(out0) if b == '1']
+        flat_primes = [s for implicant in prime_implicants for s in implicant]
+        essentials = set([e for e in numbers if flat_primes.count(e) == 1])
+        final_implicants = [s for s in prime_implicants if set(s).intersection(essentials)]
+
+        if len(prime_implicants) - len(final_implicants) > 2:
+            remainder = [s for s in prime_implicants if s not in final_implicants]
+            nonessentials = set([s for implicant in remainder for s in implicant])
+            for i in powerset(remainder):
+                if not set([a for b in i for a in b]) - nonessentials:
+                    final_implicants += list(i)
+                    break
+
+        return final_implicants
+
+    def formula(self, style="sop"):
+        """:param style: Notation options. Default is sum of product. Alternatives are 'python', 'math', 'text'."""
+
+        if style == "python":
+            andsym, orsym, notsym = '&', '|', '~'
+        elif style == "math":
+            andsym, orsym, notsym = '∧', '∨', '¬'
+        elif style == "text":
+            andsym, orsym, notsym = ' and ', 'or', 'not '
+        else:
+            andsym, orsym, notsym = '', '+', '~'
+
+        PI = self.qmc()
+        expr = ''
+        for prod in PI:
+            b = 0
+            for i in prod:
+                b |= prod[0] ^ i
+            for i in range(self.p):
+                bit = 1 << self.p - i - 1
+                if not (bit & b):
+                    if bit & ~prod[0]:
+                        expr += notsym
+                    expr += self.var_names[i] + andsym
+            if len(andsym) > 0:
+                expr = expr[:-len(andsym)]
+            expr += ' ' + orsym + ' '
+        expr = expr[:-len(orsym)-2]
+        self.expression = expr
+        return expr
